42e91bd8aa48ff4967d15ece3fc47a1a19d97172
Rana-tech <abhisek.gurung590@gmail.com>
2024-02-22 22:16:12 +0545

    completed

--- js/app.js
+++ js/app.js
@@ -1,98 +1,74 @@
-const questions = [{
-
-    'que':'Which of the following is the markup language?',
-    'a' : 'Html',
-    'b' : 'CSS',
-    'c' : 'Javascript',
-    'd' : 'PHP',
-    'ans' : 'option1'
-},
-{
-
-    'que':'Which of the following is the STYLE language?',
-    'a' : 'Html',
-    'b' : 'CSS',
-    'c' : 'Javascript',
-    'd' : 'PHP',
-    'ans' : 'option2'
-
-},
-{
-
-    'que':'Which of the following is the Backend language?',
-    'a' : 'Html',
-    'b' : 'CSS',
-    'c' : 'Javascript',
-    'd' : 'PHP',
-    'ans' : 'option4'
-
-},
-]
-let index = 0;
-let right = 0,wrong=0;
-let options = document.querySelectorAll(".option");
-let question = document.getElementById('question');
-
-let total = questions.length;
-
-const questionans = () => {
-    if (index == total){
-       return end() ;
-       }
-       reset() ;
-    let data =  questions[index];
-    question.innerHTML =  `${index + 1})  ${data.que}`;
-    options[0].nextElementSibling.innerHTML= data.a;
-    options[1].nextElementSibling.innerHTML = data.b;
-    options[2].nextElementSibling.innerHTML = data.c;
-    options[3].nextElementSibling.innerHTML = data.d;
-}
-
-const submit = () => {
-    let ans = check();
-    let data =  questions[index];
-  
-     if(ans ==data.ans){
-        right++;
-     }
-     else{
-        wrong++;
-     }
-     if(ans){
-       index++;
-
-       questionans();
-    }
-    
-  
-}
-
-const check = () => {
-    let selectedans;
-
-    
-    options.forEach(element => {
-        if(element.checked){
-             selectedans = element.value;
+const btn = document.getElementById('btn');
+const container = document.getElementById('container');
+function saveNote(){
+    const notes = document.querySelectorAll('#textarea');
+    const data = [];
+    console.log(data.length)
+    console.log("hi data")
+    notes.forEach(
+        (note)=>{
+            data.push(note.value)
         }
-    });
-    return selectedans;
+    )
+    console.log(data.length)
+    console.log("hi data")
+    if(data.length===0){
+
+        console.log("hoaisdhfhoi")
+        localStorage.removeItem("notes")
+
+    }
+    else{
+
+        localStorage.setItem("notes",JSON.stringify(data));
+    }
+
+
 }
-const reset = () => {
-    options.forEach(element => {
-       element.checked = false;
-             
-       
+btn.addEventListener("click",function(){
+    addNote()
+})
+
+/* <div class="block">
+<div class="flex topbar">
+<i class="fa-solid fa-floppy-disk" style="color:white"></i>
+<i class="fa-solid fa-trash"  style="color:white"></i>
+</div>
+<textarea name="" id="textarea" style="width: 100%;height: 100%;border: none;"></textarea>
+</div> */
+const addNote = (text="") => {
+    
+    const note = document.createElement("div");
+    note.classList.add("block");
+    note.innerHTML = `
+    <div class="flex topbar">
+    <i class="save fa-solid fa-floppy-disk" style="color:white"></i>
+    <i class="delete fa-solid fa-trash"  style="color:white"></i>
+    </div>
+    <textarea name="" id="textarea" style="width: 100%;height: 100%;border: none;">${text}</textarea>
+    `;
+    note.querySelector('.delete').addEventListener("click",function(){
+        note.remove();
     })
+    note.querySelector('.save').addEventListener("click",function(){
+        saveNote();
+    })
+    container.appendChild(note)
 }
-const end = () => {
-    document.getElementById('quiz').innerHTML = `
-    <h1> Thank you for playing </h1>
-    <h2>${right}/${total} are correct</h2>
-    `
+function start(){
+const lsNotes = JSON.parse(localStorage.getItem("notes"));
+if(lsNotes ===null)
+{
+   
+addNote()
 }
-//initial call
-questionans();
+else{
 
-
-
+    lsNotes.forEach(
+        (lsNote)=>{
+            addNote(lsNote);
+        }
+        )
+    }
+}
+start();
